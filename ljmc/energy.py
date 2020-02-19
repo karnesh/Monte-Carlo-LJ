ac300bd979ed5eb3dbd49ee6305953818b7d18cf
Karnesh Jain <jain.karnesh@gmail.com>
2020-02-18 22:00:07 -0600

    modified energy method

--- ljmc/energy.py
+++ ljmc/energy.py
@@ -16,6 +16,17 @@ def energy(atom1, atom2, rc):
     eps = atom1.eps
     sig = atom1.sigma
     rcsq = rc**2
+
+    dx = atom1.x - atom2.x
+    dy = atom1.y - atom2.y
+    dz = atom1.z - atom2.z
+
+    rsq = dx**2 + dy**2 + dz**2
+
+    if rsq <= rcsq:
+        energy = 4.0*eps*( (sig/rsq)**6.0 - (sig/rsq)**3.0)
+    else:
+        energy = 0.0
     
 
 
