e3e52e33bafac11f690329dd9cec2e05172e3205
Karnesh Jain <jain.karnesh@gmail.com>
2020-02-20 00:25:22 -0600

    Added periodic boundary conditions

--- ljmc/energy.py
+++ ljmc/energy.py
@@ -7,30 +7,41 @@ All the variables are in reduced units.
 
 """
 
-# Import libraries
-import math
-
 def distance(atom1, atom2):
     """
         Computes the square of inter particle distance
         Minimum image convention is applied for distance calculation for periodic boundary conditions
     """
+    
     dx = atom1.x - atom2.x
     dy = atom1.y - atom2.y
     dz = atom1.z - atom2.z
     
     if dx > halfLx
         dx -= Lx
-    
+    elif dx < -halfLx:
+        dx += Lx
+    if dy > halfLy:
+        dy -= Ly
+    elif dy < -halfLy:
+        dy += Ly
+    if dz > halfLz:
+        dz -= Lz
+    elif dz < -halfLz:
+        dz += Lz
     
     return dx**2 + dy**2 + dz**2
 
 
 def energy(atom1, atom2, rc):
-    '''calculates the energy of the system'''
-
-    eps = atom1.eps
-    sig = atom1.sigma
+    '''
+        calculates the energy of the system
+    '''
+    
+    ## Arithmatic mixing rules - Lorentz Berthlot mixing
+    eps = (atom1.eps + atom2.eps)/2
+    sig = (atom1.sigma * atom2.sigma)**0.5
+    
     rcsq = rc**2
 
     rsq = distance(atom1, atom2)
@@ -41,6 +52,13 @@ def energy(atom1, atom2, rc):
         energy = 0.0
     
 
+def writeEnergy(step, en):
+    '''
+        Writes the energy to a file.
+    '''
+    
+    with open('energy.dat', 'a') as f:
+        f.write('{0} {1}\n'.format(step, en))
 
 
 
